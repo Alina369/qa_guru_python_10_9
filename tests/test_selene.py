b9f848661e3f128509a5c354b9f2672a2e8a0aa4
Alina Lang <lav0903@mail.ru>
2024-03-19 15:33:30 +0300

    second commit

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -7,7 +7,8 @@ def test_github():
 
     s(".header-search-button").click()
     # browser.element(".header-search-button").type('eroshenkoam/allure-example').press_enter()
-    s(".header-search-button").send_keys("eroshenkoam/allure-example")
+    # s(".header-search-button").send_keys("eroshenkoam/allure-example")
+    browser.element("#query-builder-test").should(be.visible).send_keys('').press_enter()
     s(".header-search-button").submit()
 
     s(by.link_text("eroshenkoam/allure-example")).click()
