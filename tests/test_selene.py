070c07bbbf562ad75e9da21dd018ce1bcdf69260
Alina Lang <lav0903@mail.ru>
2024-03-19 15:28:59 +0300

    first commit

---
+++ tests/test_selene.py
@@ -0,0 +1,17 @@
+from selene import browser, be, by
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open("https://github.com")
+
+    s(".header-search-button").click()
+    # browser.element(".header-search-button").type('eroshenkoam/allure-example').press_enter()
+    s(".header-search-button").send_keys("eroshenkoam/allure-example")
+    s(".header-search-button").submit()
+
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s("#issues-tab").click()
+
+    s(by.partial_text("#76")).should(be.visible)
