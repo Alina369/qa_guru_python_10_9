030767f73e9ddfcdbaf4dfb38a203c36c33958e3
Alina Lang <lav0903@mail.ru>
2024-03-25 19:01:46 +0300

    add hw

--- tests/test_issue_name.py
+++ tests/test_issue_name.py
@@ -1,9 +1,30 @@
-import allure
-from selene import browser, be, by
 from selene.support.shared.jquery_style import s
+from selene import browser, be, by
+import allure
+from allure_commons.types import Severity
 
+# Чистый тест на Selene
+def test_git_issue_clear_selene():
+    browser.open("https://github.com")
 
-def test_dynamic_steps():
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys('eroshenkoam/allure-example').press_enter()
+
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s("#issues-tab").click()
+
+    s("#issue_76_link").should(be.visible)
+    allure.dynamic.severity(Severity.BLOCKER)
+
+# Лямбда шаги через with allure.step
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("Alina", "Lang")
+@allure.feature("Git's Issue")
+@allure.story("Пользователь видит Issue")
+@allure.link("https://github.com", name="Testing")
+def test_git_issue_with_decorator():
     with allure.step("Открываем главную страницу"):
         browser.open("https://github.com")
 
@@ -17,16 +38,17 @@ def test_dynamic_steps():
     with allure.step("Открываем таб Issue"):
         s("#issues-tab").click()
 
-    with allure.step("Проверяем наличие Issue с номером 76"):
-        s(by.partial_text("#76")).should(be.visible)
+    with allure.step("Проверяем наличие названия Issue"):
+        s("#issue_76_link").should(be.visible)
 
 
+# Шаги с декоратором @allure.step
 def test_decorator_steps():
     open_main_page()
     search_for_repository("eroshenkoam/allure-example")
     go_to_repository("eroshenkoam/allure-example")
     open_issue_tab()
-    should_see_issue_with_number("#76")
+    should_see_issue("#issue_76_link")
 
 
 @allure.step("Открываем главную страницу")
@@ -50,6 +72,6 @@ def open_issue_tab():
     s("#issues-tab").click()
 
 
-@allure.step("Проверяем наличие Issue с номером {number}")
-def should_see_issue_with_number(number):
-    s(by.partial_text(number)).click()
+@allure.step("Проверяем наличие названия Issue {name}")
+def should_see_issue(name):
+    s(name).should(be.visible)
